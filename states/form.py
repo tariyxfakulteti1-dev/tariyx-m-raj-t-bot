9e791da989f53190f5be999cdec1ee1a82f30f42
tariyxfakulteti1-dev <tariyxfakulteti1@gmail.com>
2026-08-20 21:42:25 +0500

    form.py

--- states/form.py
+++ states/form.py
@@ -2,6 +2,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AppealForm(StatesGroup):
     waiting_for_name = State()
+    waiting_for_phone = State() # Jańadan qosıldı
     waiting_for_direction = State()
     waiting_for_group = State()
     waiting_for_course = State()
